54a369a7a430892ef6b7ae3b8273c182971dd7ca
razzraziel <43266609+razzraziel@users.noreply.github.com>
2021-01-19 20:44:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ tf_mesh_renderer_path=$(pwd)/third_party/kernels/
 #sudo ldconfig
 g++ -std=c++11 \
     -shared $tf_mesh_renderer_path/rasterize_triangles_grad.cc $tf_mesh_renderer_path/rasterize_triangles_op.cc $tf_mesh_renderer_path/rasterize_triangles_impl.cc $tf_mesh_renderer_path/rasterize_triangles_impl.h \
-    -o $tf_mesh_renderer_path/rasterize_triangles_kernel.so -fPIC \
+    -o $tf_mesh_renderer_path/rasterize_triangles_kernel.so -fPIC -D_GLIBCXX_USE_CXX11_ABI=0 \
     -I$TF_INC -L$TF_LIB -ltensorflow_framework -O2
 
 if [ "$?" -ne 0 ]; then echo "compile rasterizer failed"; exit 1; fi
